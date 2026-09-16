cd613b44b738114a85555c1dc9cfaab3b21c2ab9
Ubuntu <ubuntu@ip-172-31-26-254.eu-west-1.compute.internal>
2026-09-16 15:25:31 +0000

    add model

--- src/models/train.py
+++ src/models/train.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 import joblib
+import mlflow
+import mlflow.sklearn
+
+
 
 X_train = pd.read_csv('data/processed/X_train_scaled.csv')
 y_train = pd.read_csv('data/processed/y_train.csv').values.ravel()
@@ -10,3 +14,9 @@ model = RandomForestRegressor(**best_params, random_state=42)
 model.fit(X_train, y_train)
 
 joblib.dump(model, 'models/model.pkl')
+# À la fin de ton entraînement :
+mlflow.sklearn.log_model(
+    sk_model=model, 
+    artifact_path="model", 
+    skops_trusted_types=["sklearn.tree._tree.Tree"]
+)
